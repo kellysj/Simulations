a563be82a1cb634aef239605cefe4da57e58773c
kellysj <kellyjones@gmail.com>
2015-02-24 10:08:46 -1000

    Starting a lotka volterra thing

---
+++ src/LotkaVolteraRungeKutta/LVRK.java
@@ -0,0 +1,24 @@
+package LotkaVolteraRungeKutta;
+
+public class LVRK {
+	public static void main(String[] args[]){
+		double initialX;
+		double initialY;
+		double errorLevel;
+		double x;
+		double y;
+		double delXT;
+		double delYT;
+		double endT=100;
+		double h;
+		double curT=0;
+		String outputY;
+		String outputX;
+		while(curT<=endT){
+			
+			
+		}
+	}
+	
+
+}
